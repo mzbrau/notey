af86af0208d1794e0d02654bd314e6238f01a1c2
Michael Browne <mr.michael.browne@gmail.com>
2026-05-24 21:43:51 +0200

    fix base url in docs iste

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://mzbrau.github.io',
-  baseUrl: '/notey/',
+  baseUrl: '/',
 
   organizationName: 'mzbrau',
   projectName: 'notey',
